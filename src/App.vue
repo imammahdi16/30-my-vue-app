7f7dc27aa536ac2281c03bad4a05bf25a970aa9f
imammahdi16 <165528448+imammahdi16@users.noreply.github.com>
2024-04-01 13:02:25 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <button v-on:click="increment">{{ count }}</button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
